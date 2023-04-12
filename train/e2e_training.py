4ee653ce0da6957231373880e79c18c96c6b53d4
Huynh <46445099+nguhcv@users.noreply.github.com>
2023-04-12 05:35:13 -0700

    Update e2e_training.py

--- train/e2e_training.py
+++ train/e2e_training.py
@@ -25,7 +25,7 @@ def end_to_end_learning(model_name,
     if lars_option:
         optimizer = LARSWrapper(optimizer)
 
-    scheduler = LinearWarmupCosineAnnealingLR(optimizer, warmup_epochs=10, max_epochs=3500)
+    scheduler = LinearWarmupCosineAnnealingLR(optimizer, warmup_epochs=10, max_epochs=4000)
 
 
 
